898489a6518fb5445c6f2bdb3cdf7f5ed5eedc77
MarshallWu <296396579@qq.com>
2019-12-19 14:43:10 +0800

    优化

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,16 @@
+#!/usr/bin/env sh
+
+# 确保脚本抛出遇到的错误
 set -e
 
-npm run docs:build
+# 生成静态文件
+npm run build
 
+# 进入生成的文件夹
 cd docs/.vuepress/dist
+
+#创建.nojekyll 防止Github Pages build错误
+touch .nojekyll
 
 git init
 git add -A
--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,44 +1,70 @@
+const utils = require('./utils')
+
 module.exports = {
-  title:'marshall',
-  description:'个人博客',
-  head:[
-    ['link',{rel:'icon',href:'/happy.png'}]
-  ],
-  themeConfig:{
-    //顶部导航栏
-    nav: [
-      {text: '首页', link: '/'},
+  title: 'awesome-bookmarks',
+  description: '个人收藏夹 beta',
+  base: '/awesome-bookmarks/',
+  head: [
+    [
+      'link',
       {
-        text: '分类',
-        ariaLabel: '分类',
-        items:[
-          {text: '文章', link:'/pages/folder1/test1.md'},
-          {text: '琐碎', link:'/pages/folder2/test4.md'},
-        ]
+        rel: 'icon',
+        href: '/favicon.ico'
+      }
+    ]
+  ],
+  themeConfig: {
+    nav: [
+      {
+        text: '首页',
+        link: '/'
       },
-      {text:'功能演示', link:'/pages/folder1/test3.md'},
-      {text:'Github',link:'https://github.com/dwanda'},
+      {
+        text: '库',
+        link: '/repository/'
+      },
+      {
+        text: '网站',
+        link: '/website/'
+      },
+      {
+        text: '文章',
+        link: '/article/'
+      },
+      {
+        text: '最近阅读',
+        link: '/article/read'
+      },
+      // { text: '工具', link: 'https://google.com' },
+      {
+        text: '面试',
+        link: '/interview/'
+      },
+      {
+        text: 'Blog',
+        link: '/blog/'
+      }
     ],
-    //侧边导航栏:根据当前文件路径是否匹配侧边栏数据，自动显示/隐藏
-    sidebar:{
-      '/pages/folder1/':[
-        {
-          title:'测试菜单1',
-          collapsable:false,
-          sidebarDepth:1,
-          children:[
-            ['test1.md','子菜单1'],
-            ['test3.md','子菜单2']
-          ]
-        },
-        {
-          title:'测试菜单2',
-          collapsable:false,
-          children:[
-            ['test2.md','子菜单1']  
-          ]
-        }
-      ],
+    sidebar: utils.inferSiderbars(),
+    lastUpdated: '上次更新',
+    repo: 'PanJiaChen/awesome-bookmarks',
+    editLinks: true,
+    docsDir: 'docs',
+    editLinkText: '在 GitHub 上编辑此页',
+    sidebarDepth: 3
+  },
+  configureWebpack: {
+    resolve: {
+      alias: {
+        '@public': './public'
+      }
+    }
+  },
+  ga: 'UA-109340118-1',
+  markdown: {
+    config: md => {
+      // use more markdown-it plugins!
+      md.use(require('markdown-it-include'))
     }
   }
 }
